0c8ba14d1065895abfb0d22238daaeb1338b0b15
Ana Paula Mendes <anamendest@outlook.com>
2018-04-18 19:47:57 -0400

    Parte 2

---
+++ src/SGRB.AppplicationCore/Entity/Pesquisador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGRB.AppplicationCore.Entity
+{
+    public class Pesquisador
+    {
+        public Pesquisador()
+        {
+               
+        }
+
+        public int PesquisadorId { get; set; }
+    }
+}
