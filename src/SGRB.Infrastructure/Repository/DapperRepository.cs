bf6f5778fa3460029aa511c1b09bda1cbb7f830c
Ana Paula Mendes <anamendest@outlook.com>
2018-05-30 15:01:39 -0400

    Criação do Repository.

---
+++ src/SGRB.Infrastructure/Repository/DapperRepository.cs
@@ -0,0 +1,40 @@
+﻿using SGRB.AppplicationCore.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGRB.Infrastructure.Repository
+{
+    public class DapperRepository <TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public TEntity Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Buscar(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SGRB.Infrastructure/Repository/EFRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGRB.AppplicationCore.Interfaces.Repository;
+using SGRB.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGRB.Infrastructure.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly RBasalContext _dbContext;
+
+
+        public TEntity Adicionar(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado)
+        {
+            return _dbContext.Set<TEntity>().Where(predicado).AsEnumerable();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            return _dbContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return _dbContext.Set<TEntity>().AsEnumerable();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
